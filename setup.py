a50cfa50239b19dfb3887b0640b36c2899758f71
Bradley A. Thornton <brad@ipv4sub.net>
2018-03-23 06:23:43 -0700

    More README

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='modelsettings',
-      version='1.6.2',
+      version='1.6.3',
       description='Model your application settings.',
       url='http://github.com/cidrblock/modelsettings',
       author='Bradley A. Thornton',
